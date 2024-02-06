6cb39969ba89c7cb5204c05122b150132b749eb6
Joseaholgado <115182985+Joseaholgado@users.noreply.github.com>
2024-02-06 18:59:59 +0100

    arreglos de codigo

--- borrar.sh
+++ borrar.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+#!/bin/bash
+
 # Nombre de la pila que se va a borrar
-stack_name="StackPrueba"
+stack_name="PilaTomcat"
 
 # Comando para borrar la pila
-aws cloudformation delete-stack \
-  --stack-name $stack_name
+aws cloudformation delete-stack --stack-name $stack_name
 
 # Esperar hasta que la eliminación de la pila esté completa
-aws cloudformation wait stack-delete-complete \
-  --stack-name $stack_name
+aws cloudformation wait stack-delete-complete --stack-name $stack_name
 
 echo "La pila $stack_name ha sido eliminada correctamente."
--- desplegar.sh
+++ desplegar.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
+aws cloudformation deploy --profile default --stack-name PilaTomcat --template-file ubuntu.yml
 
-# Nombre de la pila que se va a desplegar
-stack_name="StackPrueba"
-
-# Nombre del archivo YAML de la plantilla
-template_file="ubuntu.yml"
-
-# Comando para desplegar la pila
-aws cloudformation deploy \
-  --stack-name $stack_name \
-  --template-file $template_file \
-  --capabilities CAPABILITY_IAM
-   --no-fail-on-empty-changeset > errores_log.txt 2>&1
-
-# Verificar el estado de salida del comando
+#Verificar la ip
 if [ $? -eq 0 ]; then
-  echo "La pila $stack_name ha sido desplegada correctamente."
-else
-  echo "Error: No se pudo desplegar la pila $stack_name. Consulta el archivo de registro para obtener más detalles."
-fi
-
-#Verificar el ip
-
-if [ $? -eq 0 ]; then
-    aws cloudformation list-exports \
-        --query "Exports[?Name=='NOMBRE PARAMETRO DE SALIDA'].Value"
+    aws cloudformation list-exports --query "Exports[?Name=='IPInstancia'].Value" --output text
 fi
